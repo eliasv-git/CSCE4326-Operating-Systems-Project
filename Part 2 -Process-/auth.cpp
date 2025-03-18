0803a0b5187282239a50daa8133c285ad8255bcd
eliasv-git <eliasvillarreal703@gmail.com>
2025-03-17 23:39:42 -0500

    Add files via upload

---
+++ Part 2 -Process-/auth.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include "auth.h"  // Include the header file to match the declaration
+
+using namespace std;
+
+// Define the authenticateUser function
+bool authenticateUser() {
+    string username, password;
+
+    // Hardcoded credentials for authentication
+    const string correctUsername = "admin";
+    const string correctPassword = "password123";
+
+    cout << "\nWelcome to the system!" << endl;
+    cout << "Please Sign In to continue." << endl;
+
+    while(true){
+    // Prompt user for input
+    cout << "Enter your username: ";
+    cin >> username;
+    cout << "Enter your password: ";
+    cin >> password;
+
+    // Check if the input matches the hardcoded credentials
+    if (username == correctUsername && password == correctPassword) {
+        cout << "Authentication successful!" << endl;
+        return true;  // Authentication success
+    }; 
+        cout << "Invalid username or password!" << endl; //Authentication Failed
+    };
+}
